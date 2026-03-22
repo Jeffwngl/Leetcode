c883555d2f74fc3e1d9633dec8a318c563280ff7
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-03-23 10:54:49 +1100

    edit distance

---
+++ 72/Edit-distance.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+
+        if (n == 0 || m == 0) {
+            return max(n, m);
+        }
+
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+
+        int cnt = 0;
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = cnt;
+            cnt++;
+        }
+
+        cnt = 0;
+        for (int i = 0; i <= m; ++i) {
+            dp[0][i] = cnt;
+            cnt++;
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = min(1 + dp[i][j - 1], min(1 + dp[i - 1][j], 1 + dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
